13275a576e4edfe8afd26c12f2cbaaf9352fa5cb
Grimstroke <shukurova_darya@icloud.com>
2024-09-15 13:48:21 +0500

    start progect

--- gulp/copyAssets.mjs
+++ gulp/copyAssets.mjs
@@ -1,20 +1,22 @@
 import gulp from 'gulp';
 
 const copySvg = () =>
-  gulp.src('source/img/**/*.svg', {base: 'source'})
+  gulp.src('source/img//*.svg', {base: 'source'})
       .pipe(gulp.dest('build'));
 
 const copyImages = () =>
-  gulp.src('source/img/**/*.{png,jpg,webp}', {base: 'source'})
+  gulp.src('source/img//*.{png,jpg,webp}', {base: 'source'})
       .pipe(gulp.dest('build'));
 
 const copy = () =>
   gulp.src([
-    'source/**.html',
-    'source/fonts/**',
-    'source/img/**',
+    'source/.html',
+    'source/fonts/',
+    'source/img/',
     'source/favicon.ico',
-    'source/manifest.json'
+    'source/manifest.json',
+    'source/**.png',
+    'source/**.svg',
   ], {
     base: 'source',
   })
